032369939cafe1f411ceb4216a6ca57b6c2dd71b
Roberto <Roberto@DESKTOP-0AGIUPU>
2023-12-21 08:58:31 -0300

    Add project files.

---
+++ Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FinancialApp.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Informe o título")]
+        public string Title { get; set; } = string.Empty;
+    }
+}
---
+++ Models/Expense.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FinancialApp.Models
+{
+    public class Expense
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required(ErrorMessage = "Informe o tipo do gasto!")]
+        public Category Category { get; set; }
+        [Required(ErrorMessage = "Informe o nome do gasto!")]
+        public string Name { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
+        public double Value { get; set; }
+
+    }
+}
